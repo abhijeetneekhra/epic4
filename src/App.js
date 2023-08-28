94600e6db02ca34f5b41cd34fb473be77af8ed57
abhijeetneekhra <aknwaits4u@gmail.com>
2023-08-28 17:32:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Login from "./Login";
 import Register from "./pages/Register";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ForgotPassword from "./pages/Forgot-password";
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
